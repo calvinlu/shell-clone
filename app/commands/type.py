38bed9a0ac2b742b22996122ceb194399e190a02
Calvin Lu <calvinlu2015@gmail.com>
2025-12-21 22:10:20 -0800

    CLI tests (cli-test-1766383820394)

---
+++ app/commands/type.py
@@ -0,0 +1,15 @@
+"""
+A command that checks whether a command is a builtin, an executable file, or unrecognized.
+"""
+TYPE_COMMAND = "type"
+
+VALID_BUILTINS = ["echo", "type", "exit"]
+
+def command_type(args: list[str]):
+    """ Type the arguments """
+    if len(args) == 0:
+        print("type: missing operand")
+        return
+    argument = args[0]
+    print(f"{argument} is a builtin command" if argument in
+     VALID_BUILTINS else f"{argument} not found")
