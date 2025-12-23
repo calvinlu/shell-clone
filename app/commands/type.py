51c3f15a495906d465c6639d0f73a316a17315c2
Calvin Lu <calvinlu2015@gmail.com>
2025-12-23 00:46:32 -0800

    CLI tests (cli-test-1766479592659)

--- app/commands/type.py
+++ app/commands/type.py
@@ -1,7 +1,7 @@
 """
 A command that checks whether a command is a builtin, an executable file, or unrecognized.
 """
-import os
+from app.utils import find_executable
 
 TYPE_COMMAND = "type"
 
@@ -27,19 +27,3 @@ def command_type(args: list[str]):
 def is_builtin(argument: str) -> bool:
     """Checks if argument is a builtin"""
     return argument in VALID_BUILTINS
-
-
-def find_executable(argument: str) -> str:
-    """Find the full path to an executable in PATH"""
-    if os.path.isabs(argument):
-        if os.path.isfile(argument) and os.access(argument, os.X_OK):
-            return argument
-        return ""
-
-    for path in os.environ.get("PATH", "").split(os.pathsep):
-        if not path:
-            continue
-        full_path = os.path.join(path, argument)
-        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-            return full_path
-    return ""
