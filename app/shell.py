69258410c66e541bfa4da118475e4e99f00874f8
Calvin Lu <calvinlu2015@gmail.com>
2025-12-23 01:01:10 -0800

    Implement pwd

--- app/shell.py
+++ app/shell.py
@@ -4,11 +4,12 @@ A simple shell implementation that provides a basic command-line interface.
 This module implements a minimal shell that can be extended with additional commands.
 Currently supports the 'exit' command to terminate the shell.
 """
-import os
+
 import subprocess
 import sys
 from app.commands.echo import command_echo
 from app.commands.type import command_type
+from app.commands.pwd import command_pwd
 from app.utils import find_executable
 
 def shell():
@@ -26,7 +27,7 @@ def shell():
             with subprocess.Popen([command] + args) as process:
                 process.wait()
             continue
-        
+
         match command:
             case "exit":
                 break
@@ -34,6 +35,8 @@ def shell():
                 command_echo(args)
             case "type":
                 command_type(args)
+            case "pwd":
+                command_pwd()
             case _:
                 print(f"{command}: command not found")
                 continue
